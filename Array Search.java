14a38802156692bddb4a34475bfb86be893bea31
Reeman Abd Alqader <eng.reeman.m.a@gmail.com>
2025-07-12 09:40:38 +0300

    Create Array Search.java

---
+++ Array Search.java
@@ -0,0 +1,11 @@
+//https://www.geeksforgeeks.org/problems/search-an-element-in-an-array-1587115621/1
+class Solution {
+    static int search(int arr[], int x) {
+        for (int i=0;i<arr.length ;i++){
+            if (arr[i]==x){
+                return i ;
+            }
+        }
+        return -1 ;
+    }
+}
